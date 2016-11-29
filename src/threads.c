ba65ffbb2d2fe0733d0538bb206a25f1ac66b045
Ilya Kaysin <demarkok@gmail.com>
2016-11-29 11:38:13 +0300

    delete usless variable

--- src/threads.c
+++ src/threads.c
@@ -1,17 +1,12 @@
 #include "threads.h"
 
-int claim = 0;
 
 // threads manager works with interruptions, so we can just diable interruptions to lock 'mutex'  
 
 void lock() {
-	if (claim == 0) {
-		__asm__ volatile("cli"); // disable interrupts
-	}
-	claim = 1;
+	__asm__ volatile("cli"); // disable interrupts
 }
 
 void unlock() {
-	claim = 0;
 	__asm__ volatile("sti"); // enable interrupts
 }
